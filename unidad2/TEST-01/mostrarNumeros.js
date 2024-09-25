be3ad1bd06f1f3db0b4c4f9e04f89e408b168be1
XxSebasx <sebastian.garcia.recio.alu@iesfernandoaguilar.es>
2024-09-25 12:29:32 +0200

    otro commit

---
+++ unidad2/TEST-01/mostrarNumeros.js
@@ -0,0 +1,13 @@
+//ejercio tarea 1
+
+let entero = 10;
+let decimal = 3.14;
+let cientifico = 1.23e5;
+let octal = 0o12;
+let hexadecimal = 0x1A;
+
+alert("Numeros entero: " + entero);
+alert("Numero decimal: " + decimal);
+alert("Numero cientifico: " + cientifico);
+alert("Numero octal: " + octal);
+alert("Numero hexadecimal: " + hexadecimal);
--- unidad2/TEST-01/script.js
+++ unidad2/TEST-01/script.js
@@ -1,9 +1,10 @@
 var i,
-j = 0;
-    for (i = 0; i < 5; i++) {
-        alert("Variable i: " + i);
-        for (let j = 0; j < 5; j++) {
-            if (i % 2 == 0) {
-                document.write(i + "-" + "<br>");
-        }   }
+    j = 0;
+for (i = 0; i < 5; i++) {
+    alert("Variable i: " + i);
+    for (let j = 0; j < 5; j++) {
+        if (i % 2 == 0) {
+            document.write(i + "-" + "<br>");
+        }
     }
+}
