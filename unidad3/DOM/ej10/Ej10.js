14c20c5aebd6f2241c24e69553151e993db4fd90
XxSebasx <125697490+XxSebasx@users.noreply.github.com>
2024-11-21 13:57:39 +0100

    Add files via upload

--- unidad3/DOM/ej10/Ej10.js
+++ unidad3/DOM/ej10/Ej10.js
@@ -1,10 +1,25 @@
 function comprobar() {
-    let correo = document.getElementById('correo').value;
-    let web = document.getElementById('web').value;
-}
+    let correo = document.getElementById('email');
+    let web = document.getElementById('url');
+    let parrafo = document.getElementsByTagName("p")
+    if(correo == null || !correo.checkValidity()) {
+        parrafo[0].appendChild(document.createElement("p").appendChild(document.createTextNode("correo invalido")));
+    }else{
+        parrafo[0].appendChild(document.createElement("p").appendChild(document.createTextNode("correcto")));
+    }
 
-function incio() {
-    document.getElementById("confirmar").addEventListener("click",comprobar)    
+    if(web == null || !web.checkValidity()) {
+        parrafo[1].appendChild(document.createElement("p").appendChild(document.createTextNode("Url invalida")));
+    }else{
+        parrafo[1].appendChild(document.createElement("p").appendChild(document.createTextNode("correcto")));
+    }
+    
+}
+function inicio() {
+    document.getElementById("formulario").addEventListener("submit", (event) => {
+        event.preventDefault();
+        comprobar();
+    });
 }
 
 window.onload = inicio;
